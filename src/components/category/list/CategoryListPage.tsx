b89b06540c1a8ebfcbd47a020a2d93a23268b163
MaxRomantsov <Roma_ua72@student.itstep.org>
2024-04-01 20:21:45 +0300

    Start with products

--- src/components/category/list/CategoryListPage.tsx
+++ src/components/category/list/CategoryListPage.tsx
@@ -1,6 +1,6 @@
-import {Button, Col, Form, Input, Pagination, Row} from "antd";
+import {Button, Col, Collapse, Form, Input, Pagination, Row} from "antd";
 import {Link, useSearchParams} from "react-router-dom";
-import {ICategoryCreate, ICategorySearch, IGetCategories} from "../types.ts";
+import {ICategorySearch, IGetCategories} from "../types.ts";
 import http_common from "../../../http_common.ts";
 import {useEffect, useState} from "react";
 import CategoryCard from "./CategoryCard.tsx";
@@ -8,17 +8,15 @@ import CategoryCard from "./CategoryCard.tsx";
 const CategoryListPage = () => {
 
     const [data, setData] = useState<IGetCategories>({
-        content: [],
-        totalPages: 0,
-        totalElements: 0,
-        number: 0
+        list: [],
+        totalCount: 0
     });
 
     const [searchParams, setSearchParams] = useSearchParams();
     const [formParams, setFormParams] = useState<ICategorySearch>({
         keyword: searchParams.get('keyword') || "",
         page: Number(searchParams.get('page')) || 1,
-        size: Number(searchParams.get('size')) || 1
+        size: Number(searchParams.get('size')) || 8
     });
 
     const [form] = Form.useForm<ICategorySearch>();
@@ -31,7 +29,7 @@ const CategoryListPage = () => {
         const fetchData = async () => {
             try {
                 const response =
-                    await http_common.get<IGetCategories>(`/api/categories/search?keyword=${formParams.keyword}&page=${(formParams.page-1)}&size=${formParams.size}`);
+                    await http_common.get<IGetCategories>(`/api/categories/search?keyword=${formParams.keyword}&page=${(formParams.page - 1)}&size=${formParams.size}`);
                 console.log("response.data", response.data)
                 setData(response.data);
                 // setLoading(false);
@@ -44,16 +42,16 @@ const CategoryListPage = () => {
         fetchData();
     }, [JSON.stringify(formParams)]);
 
-    const {content, totalPages, totalElements, number } = data;
+    const {list, totalCount} = data;
 
     const handleDelete = async (categoryId: number) => {
         try {
             await http_common.delete(`/api/categories/${categoryId}`);
-            setData({ ...data, content: content.filter(x => x.id != categoryId)});
+            setData({...data, list: list.filter(x => x.id != categoryId)});
         } catch (error) {
             throw new Error(`Error: ${error}`);
         }
-    };
+    }
     const handlePageChange = async (page: number, newPageSize: number) => {
         findCategories({...formParams, page, size: newPageSize});
     };
@@ -62,7 +60,7 @@ const CategoryListPage = () => {
         updateSearchParams(model);
     }
 
-    const updateSearchParams = (params : ICategorySearch) =>{
+    const updateSearchParams = (params: ICategorySearch) =>{
         for (const [key, value] of Object.entries(params)) {
             if (value !== undefined && value !== 0) {
                 searchParams.set(key, value);
@@ -81,45 +79,65 @@ const CategoryListPage = () => {
                 </Button>
             </Link>
 
-            <Row gutter={16}>
-                <Form form={form}
-                      onFinish={onSubmit}
-                      layout={"vertical"}
-                      style={{
-                          minWidth: '100%',
-                          display: 'flex',
-                          flexDirection: 'column',
-                          justifyContent: 'center',
-                          padding: 20,
-                      }}
-                >
-                    <Form.Item
-                        label="Назва"
-                        name="keyword"
-                        htmlFor="keyword"
+            <Collapse defaultActiveKey={0}>
+                <Collapse.Panel key={1} header={"Панель пошуку"}>
+                    <Row gutter={16}>
+                        <Form form={form}
+                              onFinish={onSubmit}
+                              layout={"vertical"}
+                              style={{
+                                  minWidth: '100%',
+                                  display: 'flex',
+                                  flexDirection: 'column',
+                                  justifyContent: 'center',
+                                  padding: 20,
+                              }}
                     >
-                        <Input autoComplete="keyword"/>
-                    </Form.Item>
+                            <Form.Item
+                                label="Назва"
+                                name="keyword"
+                                htmlFor="keyword"
+                            >
+                                <Input autoComplete="keyword"/>
+                            </Form.Item>
 
-                    <Row style={{display: 'flex', justifyContent: 'center'}}>
-                        <Button style={{margin: 10}} type="primary" htmlType="submit">
-                            Пошук
-                        </Button>
-                        <Button style={{margin: 10}} htmlType="button" onClick={() =>{ }}>
-                            Скасувати
-                        </Button>
+                            <Row style={{display: 'flex', justifyContent: 'center'}}>
+                                <Button style={{margin: 10}} type="primary" htmlType="submit">
+                                    Пошук
+                                </Button>
+                                <Button style={{margin: 10}} htmlType="button" onClick={() => {
+                                }}>
+                                    Скасувати
+                                </Button>
+                            </Row>
+                        </Form>
                     </Row>
-                </Form>
+                </Collapse.Panel>
+            </Collapse>
+
+            <Row style={{width: '100%', display: 'flex', marginTop: '25px', justifyContent: 'center'}}>
+                <Pagination
+                    showTotal={(total, range) => {
+                        console.log("range ", range);
+                        return (`${range[0]}-${range[1]} із ${total} записів`);
+                    }}
+                    current={formParams.page}
+                    pageSize={formParams.size}
+                    total={totalCount}
+                    onChange={handlePageChange}
+                    pageSizeOptions={[4, 8, 12, 20]}
+                    showSizeChanger
+                />
             </Row>
 
             <Row gutter={16}>
                 <Col span={24}>
                     <Row>
-                        {data.content.length === 0 ? (
+                        {list.length === 0 ? (
                             <h2>Список пустий</h2>
                         ) : (
-                            data.content.map((item) =>
-                                <CategoryCard key={item.id} item={item} handleDelete={handleDelete} />,
+                            list.map((item) =>
+                                <CategoryCard key={item.id} item={item} handleDelete={handleDelete}/>,
                             )
                         )}
                     </Row>
@@ -132,11 +150,11 @@ const CategoryListPage = () => {
                         console.log("range ", range);
                         return (`${range[0]}-${range[1]} із ${total} записів`);
                     }}
-                    current={(number+1)}
-                    defaultPageSize={formParams.size}
-                    total={totalElements}
+                    current={formParams.page}
+                    pageSize={formParams.size}
+                    total={totalCount}
                     onChange={handlePageChange}
-                    pageSizeOptions={[1, 2, 5, 10]}
+                    pageSizeOptions={[4, 8, 12, 20]}
                     showSizeChanger
                 />
             </Row>
